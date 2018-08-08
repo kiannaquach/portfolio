565d0658a8b9baed356a00dcfc9f0731c925b957
Kianna <kiannaquach.kq@gmail.com>
2018-08-07 21:10:03 -0700

    Fix about

--- src/About.js
+++ src/About.js
@@ -4,12 +4,11 @@ import './App.css';
 class About extends Component {
   render() {
     return (
-      <div>
+      <div style={{backgroundColor: `${this.props.color}`}}>
           <div>
-            <img src="https://s3-us-west-1.amazonaws.com/kianna-portfolio/kianna2.jpg" className="rounded-circle" width="240" height="240" alt="profile" /> 
+            <img src="https://s3-us-west-1.amazonaws.com/kianna-portfolio/kianna2.jpg" className="rounded-circle" width="200" height="200" alt="profile" /> 
           </div>
-          <br />
-          <div style={{fontSize: '16pt', textAlign: 'left'}}>
+          <div style={{fontSize: '14pt', textAlign: 'left'}}>
             <div>
             I am a <span style={{color: 'rgb(48, 110, 227)', fontWeight: 400}}>Fullstack Engineer</span> looking for new opportunities. 
             I am skilled in <span style={{color: 'rgb(48, 110, 227)', fontWeight: 400}}>Javascript</span>, Postgres, Mongo, <span style={{color: 'rgb(48, 110, 227)', fontWeight: 400}}>React</span>, Node.js/Express and much more. 
